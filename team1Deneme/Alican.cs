14669b8e5d6f05c1097f1573d413922808e71dad
Alican Dökmen <alican@outlook.com>
2024-03-21 13:47:54 +0300

    class eklendi

---
+++ team1Deneme/Alican.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team1Deneme
+{
+    public class Alican
+    {
+        public int Alican { get; set; }
+    }
+}
