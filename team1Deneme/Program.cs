6c17a6bbd58c276c4bf7eb276da47b8eecda160d
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2024-03-21 10:29:40 +0300

    Add project files.

---
+++ team1Deneme/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace team1Deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
